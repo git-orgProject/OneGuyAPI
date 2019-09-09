b50dfa23b18c7e4a3dde28592685b3ea2d8c8d64
Mr.zfl <1094563328@qq.com>
2019-09-09 20:24:53 +0800

    1

--- goods/models.py
+++ goods/models.py
@@ -3,18 +3,19 @@ from django.db import models
 # Create your models here.
 from common import YGBaseModel
 
+
 class CategoryEntity(YGBaseModel):
-    code=models.UUIDField(max_length=20,
-                          verbose_name='编码')
-    name=models.CharField(max_length=20,
-                          verbose_name='名称')
-    grade=models.IntegerField(default=1,
-                              verbose_name='等级')
-    parent=models.ForeignKey('self',
-                             on_delete=models.CASCADE,
-                             verbose_name='父类',
-                             null=True,
-                             blank=True)
+    code = models.UUIDField(max_length=20,
+                            verbose_name='编码')
+    name = models.CharField(max_length=20,
+                            verbose_name='名称')
+    grade = models.IntegerField(default=1,
+                                verbose_name='等级')
+    parent = models.ForeignKey('self',
+                               on_delete=models.CASCADE,
+                               verbose_name='父类',
+                               null=True,
+                               blank=True)
     picture_url = models.CharField(max_length=200,
                                    verbose_name='图片路径',
                                    blank=True,
