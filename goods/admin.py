3eca070be36ec92abae94a7690a6bd38425d7b42
Ivan-xia <875634410@qq.com>
2019-09-09 20:08:11 +0800

    我修改了商品模型数据

--- goods/admin.py
+++ goods/admin.py
@@ -11,11 +11,11 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(CategoryEntity, CategoryAdmin)
 
 class CommodityAdmin(admin.ModelAdmin):
-    list_display = ('categoryId','commodityName','state','sellPrice',
-                    'maxCommodityCount','sales','spec','field','iframepage',
-                    'smallPicture','showPicture','subTitle','canAddCart','canNoReasonToReturnText','deliveryTips')
-    fields = ('categoryId','commodityName','state','sellPrice',
+    list_display = ('categoryId_id','commodityName','state','sellPrice',
                     'maxCommodityCount','sales','spec','field','iframepage',
                     'smallPicture','showPicture','subTitle','canAddCart')
-    search_fields = ('commodityName')
+    fields = ('categoryId','commodityName','state','sellPrice',
+                    'maxCommodityCount','sales','spec','field','iframepage',
+                    'smallPicture','showPicture','subTitle','canAddCart','canNoReasonToReturnText','deliveryTips')
+    search_fields = ('commodityName',)
 admin.site.register(Commodity, CommodityAdmin)
--- goods/models.py
+++ goods/models.py
@@ -11,7 +11,7 @@ class CategoryEntity(YGBaseModel):
     grade=models.IntegerField(default=1,
                               verbose_name='等级')
     parent=models.ForeignKey('self',
-                             on_delete=models.CASCADE())
+                             on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -23,21 +23,21 @@ class CategoryEntity(YGBaseModel):
 
 class Commodity(models.Model):  # 要继承
     __commodityTuple__ = ((0, "无货"), (1, "有货"))
-    categoryId = models.ForeignKey(CategoryEntity, on_delete="SET_NULL", blank=True, null=True, verbose_name="分类id")
-    commodityName = models.CharField(max_length="200", verbose_name="商品名称")
+    categoryId = models.ForeignKey(Category, on_delete="SET_NULL", blank=True, null=True, verbose_name="分类id")
+    commodityName = models.CharField(max_length=200, verbose_name="商品名称")
     state = models.IntegerField(choices=__commodityTuple__, verbose_name="商品状态")
     sellPrice = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="销售价格")
     maxCommodityCount = models.IntegerField(null=True, blank=True, verbose_name="库存")
     sales = models.IntegerField(null=True, blank=True, verbose_name="销量")
-    spec = models.CharField(max_length="50", verbose_name="商品规格")
-    field = models.CharField(max_length="100", verbose_name="产地")
-    iframepage = models.CharField(max_length="400", null=True, blank=True, verbose_name="图文详情")
-    smallPicture = models.CharField(max_length="400", verbose_name="商品小图")
-    showPicture = models.CharField(max_length="400", verbose_name="商品展示图片")
-    subTitle = models.CharField(max_length="200", null=True, blank=True, verbose_name="商品二级标题")
+    spec = models.CharField(max_length=50, verbose_name="商品规格")
+    field = models.CharField(max_length=100, verbose_name="产地")
+    iframepage = models.CharField(max_length=400, null=True, blank=True, verbose_name="图文详情")
+    smallPicture = models.CharField(max_length=400, verbose_name="商品小图")
+    showPicture = models.CharField(max_length=400, verbose_name="商品展示图片")
+    subTitle = models.CharField(max_length=200, null=True, blank=True, verbose_name="商品二级标题")
     canAddCart = models.BooleanField(verbose_name="能否加入购物车")
-    canNoReasonToReturnText = models.CharField(max_length="100", default="不支持7天无理由退货")
-    deliveryTips = models.CharField(max_length="100", default=" 16:00 前完成订单，预计明日送达")
+    canNoReasonToReturnText = models.CharField(max_length=100, default="不支持7天无理由退货",null=True, blank=True,)
+    deliveryTips = models.CharField(max_length=100, default=" 16:00 前完成订单，预计明日送达",null=True, blank=True,)
 
     def __str__(self):
         return self.commodityName
