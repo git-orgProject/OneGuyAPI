ceaec6a385109bdc3673b17a42e2a1c209731470
Mr.zfl <1094563328@qq.com>
2019-09-09 19:40:38 +0800

    ..

--- cart/models.py
+++ cart/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+
+from common import YGBaseModel
 from user.models import UserModel
-from goods.models import Commodity
+from goods.models import CommondityModel
 # Create your models here.
-class CartModel(models.Model):
+
+
+class CartModel(YGBaseModel):
     user_id = models.OneToOneField(UserModel,
                                    verbose_name='用户ID',
                                    on_delete=models.CASCADE)
@@ -12,10 +16,10 @@ class CartModel(models.Model):
         verbose_name = '购物车'
 
 
-class CommondityCart(models.Model):
+class CommondityCart(YGBaseModel):
     cart = models.ForeignKey(CartModel,
                              on_delete=models.CASCADE,
-                             verbose_name='购物车')
+                             verbose_name='购物车ID')
 
     commondity = models.ForeignKey(CommondityModel,
                                    on_delete=models.CASCADE,
