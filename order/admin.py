952b1dafa2609717143078c2941732f0bfa97762
wdpedwin <wdpedwin@163.com>
2019-09-09 17:55:19 +0800

    addcity

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from order.models import CityModel, AreaModel
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('cityName','firstChar')
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('AreaName','city')
+
+
+
+admin.site.register(CityModel,CityAdmin)
+admin.site.register(AreaModel,AreaAdmin)
--- order/models.py
+++ order/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from common import YGBaseModel
 
 # Create your models here.
+
+class CityModel(YGBaseModel):
+    cityName = models.CharField(max_length=20,verbose_name='城市名')
+    firstChar = models.CharField(max_length=2,verbose_name='首字母')
+    def __str__(self):
+        return self.cityName
+    class Meta:
+        db_table = 't_city'
+        verbose_name_plural = verbose_name = '城市'
+
+class AreaModel(YGBaseModel):
+    AreaName = models.CharField(max_length=20,verbose_name='地区名')
+    city = models.ForeignKey(CityModel,related_name='area',
+                             on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.AreaName
+    class Meta:
+        db_table = 't_area'
+        verbose_name_plural = verbose_name = '地区'
+
+
+
+
+
+
+
