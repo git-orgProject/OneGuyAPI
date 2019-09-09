76e10e7aef10e89eaa6ae02c4bff7c75794acd20
liangjiaxing0316 <1559458506@qq.com>
2019-09-09 18:04:22 +0800

    'user'

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import UserModel
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id','phone' ,'user_name', 'sex' ,'password')
+    list_per_page = 2
+
+
+admin.site.register(UserModel,UserAdmin)
