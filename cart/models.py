106082a9ff9c2213b0d67de063bc7084470c742c
Mr.zfl <1094563328@qq.com>
2019-09-09 19:16:52 +0800

    购物车

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from user.models import UserModel
+from goods.models import Commodity
 # Create your models here.
+class CartModel(models.Model):
+    user_id = models.OneToOneField(UserModel,
+                                   verbose_name='用户ID',
+                                   on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 't_cart'
+        verbose_name = '购物车'
+
+
+class CommondityCart(models.Model):
+    cart = models.ForeignKey(CartModel,
+                             on_delete=models.CASCADE,
+                             verbose_name='购物车')
+
+    commondity = models.ForeignKey(CommondityModel,
+                                   on_delete=models.CASCADE,
+                                   verbose_name='商品ID')
+    count = models.IntegerField(verbose_name='商品数量',
+                                default=1)
+
+    class Meta:
+        db_table = 't_goods_cart'
+        verbose_name_plural = verbose_name = '购物车详情表'
