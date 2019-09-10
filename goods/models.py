03a0e29a5d562e2d7073815c7ac0a034897856c5
GunnWong <25360051@qq.com>
2019-09-10 17:30:54 +0800

    '1s'

--- goods/models.py
+++ goods/models.py
@@ -7,26 +7,27 @@ from common import YGBaseModel
 
 
 class CategoryModel(YGBaseModel):
-    code=models.CharField(max_length=20,
-                          verbose_name='编码')
-    name=models.CharField(max_length=20,
-                          verbose_name='名称')
-    grade=models.IntegerField(default=1,
-                              verbose_name='等级')
-    parent=models.ForeignKey('self',
-                             on_delete=models.CASCADE,
-                             verbose_name='父类',
-                             null=True,
-                             blank=True)
+    code = models.CharField(max_length=20,
+                            verbose_name='编码')
+    name = models.CharField(max_length=20,
+                            verbose_name='名称')
+
+    grade = models.IntegerField(default=1,
+                                verbose_name='等级')
+
+    parent = models.ForeignKey('self',
+                               on_delete=models.CASCADE,
+                               verbose_name='父类',
+                               null=True,
+                               blank=True)
 
     picture_url = models.CharField(max_length=200,
                                    verbose_name='图片路径',
                                    blank=True,
                                    null=True)
 
-
     def __str__(self):
-        return self.id
+        return self.name
 
     class Meta:
         db_table = 't_category'
