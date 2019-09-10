f968afd559749516d288117984bce9a76329f06b
GunnWong <25360051@qq.com>
2019-09-10 20:24:11 +0800

    '增加分类api'

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@
 from rest_framework import routers
 
 from .cart import CartAPIView, CommodityCartAPIView
+from .goods import CategoryAPIView
 
 # 声明api路由
 api_router = routers.DefaultRouter()
@@ -10,3 +11,4 @@ api_router = routers.DefaultRouter()
 # 向api路由中注册ViewSet
 api_router.register('cart', CartAPIView)
 api_router.register('CommodityCart', CommodityCartAPIView)
+api_router.register('Category',CategoryAPIView)
