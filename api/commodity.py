7ebd9b0b72852f4e342a76bd879298ae0be3533b
Ivan-xia <875634410@qq.com>
2019-09-10 21:22:40 +0800

    1

--- api/commodity.py
+++ api/commodity.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers, viewsets
 from goods.models import CommodityModel
-
+from .category import CategorySerializer
 
 # 序列化类
 class CommodityModelSerializer(serializers.HyperlinkedModelSerializer):
+    categoryId = CategorySerializer
     class Meta:
         model = CommodityModel
-        fields = ('id','commodityName', 'state','sellPrice',
+        fields = ('id','categoryId','commodityName', 'state','sellPrice',
                   'maxCommodityCount','spec','smallPicture','showPicture',
                   'subTitle','canAddCart','canNoReasonToReturnText','deliveryTips')
 
