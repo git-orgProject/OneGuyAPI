3925511668c7b240be72048da3ad8fbaaec80ed7
liangjiaxing0316 <1559458506@qq.com>
2019-09-10 23:53:00 +0800

    'login'

--- user/models.py
+++ user/models.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.hashers import make_password
 from django.db import models
+from django import forms
 
 class UserModel(models.Model):
-    user_id = models.IntegerField(max_length=20,
-                                  verbose_name='用户ID')
+    user_id = models.CharField(max_length=20,
+                                  verbose_name='用户ID',
+                                  unique=True)
 
-    phone = models.IntegerField(max_length=11,
+    phone = models.CharField(max_length=11,
                                 verbose_name='手机号码',
                                 blank=True,
-                                null=True,)
+                                null=True,
+                                unique=True)
 
     user_name = models.CharField(max_length=20,
                                  verbose_name='昵称')
@@ -23,12 +26,12 @@ class UserModel(models.Model):
                                 blank=True,
                                 null=True)
 
-    def save(self,force_insert=False,force_update=False,using=None,update_field=None):
-        if len(self.password)<10:
-            #明文转密文
-            self.password = make_password(self.password)
-
-        super().save()
+    # def save(self,force_insert=False,force_update=False,using=None,update_field=None):
+    #     if len(self.password)<10:
+    #         #明文转密文
+    #         self.password = make_password(self.password)
+    #
+    #     super().save()
 
     def __str__(self):
         return self.user_name
@@ -38,4 +41,9 @@ class UserModel(models.Model):
         verbose_name_plural = verbose_name = '客户管理'
 
 
+
+
+class UserForm(forms.Form):
+        username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from user import views
 
 app_name = 'user'
 
+
 urlpatterns = [
+    path('index/', views.index),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('logout/', views.logout),
 
 ]
