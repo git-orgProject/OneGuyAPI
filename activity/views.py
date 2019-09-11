df91f35930f11e86ea7b2b9cccdbacfe2fcc6bf7
wdpedwin <wdpedwin@163.com>
2019-09-11 09:48:45 +0800

    'apijiekou'

--- activity/views.py
+++ activity/views.py
@@ -4,14 +4,28 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from order.models import CityModel
-from api.active import CityModelSerializer
+from activity.models import NavlistModel
+from order.models import CityModel, AreaModel
+from api.active import CityModelSerializer, navModelSerializer, AreaModelSerializer
 
 
 class CityView(APIView):
     def get(self,request):
         datas = CityModel.objects.all()
         serializer = CityModelSerializer(datas,many=True)
+        return Response(serializer.data)
+
+class NavlistView(APIView):
+    def get(self,request):
+        datas = NavlistModel.objects.all()
+        serializer=navModelSerializer(datas,many=True)
+        return Response(serializer.data)
+
+class AreaModelView(APIView):
+    def get(self,request):
+        citys = CityModel.objects.filter(cityName='西安').first()
+        datas = AreaModel.objects.filter(city_id=citys.id).all()
+        serializer=AreaModelSerializer(datas,many=True)
         return Response(serializer.data)
 
 
--- api/active.py
+++ api/active.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from order.models import CityModel
+
+from activity.models import NavlistModel
+from order.models import CityModel, AreaModel
+
+
+class AreaModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = AreaModel
+        fields=('id','AreaName','city_id')
 
 class CityModelSerializer(serializers.HyperlinkedModelSerializer):
+    area = AreaModelSerializer(many=True)
     class Meta:
         model = CityModel
-        fields=('id','cityName','firstChar')
+        fields=('id','cityName','firstChar','area')
+
+class navModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NavlistModel
+        fields=('id','navName','navPictureUrl','navLinkUrl')
+
