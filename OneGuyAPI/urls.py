41ec018782d730735126148d4e0bf4309f326ce9
wdpedwin <wdpedwin@163.com>
2019-09-11 09:53:13 +0800

    'apijiekou'

--- OneGuyAPI/urls.py
+++ OneGuyAPI/urls.py
@@ -25,12 +25,9 @@ urlpatterns = [
     path('send_html', mail.send_html),
     path('api/', include(api_router.urls)),
     path('user/', include('user.urls', namespace='user')),
-    path('goods/', include('goods.urls',namespace='goods')),
     path('order/', include('order.urls',namespace='order')),
     path('cart/', include('cart.urls',namespace='cart')),
     path('active/', include('activity.urls',namespace='activity')),
     path('goods/', include('goods.urls', namespace='goods')),
-    path('order/', include('order.urls', namespace='order')),
-    path('cart/', include('cart.urls', namespace='cart')),
 ]+static(settings.MEDIA_URL,
          document_root=settings.MEDIA_ROOT)
