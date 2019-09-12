0b6934bdf3f5b5ab84c3982d09adbe4a82c59b2b
Ivan-xia <875634410@qq.com>
2019-09-12 10:30:56 +0800

    完成了所有的单元测试

---
+++ goods/test_/test_commodity.py
@@ -0,0 +1,31 @@
+import random
+import unittest
+from unittest import TestCase
+import requests
+
+test_data = {
+
+}
+
+class CommodityTestCase(TestCase):
+
+    def test_commodity(self):
+        url = 'http://localhost:8000/goods/commodity/'
+        resp = requests.get(url)
+        commodity_list = resp.json().get('data')
+        print(commodity_list)
+
+    def test_commoditypk(self):
+        url = 'http://localhost:8000/goods/commoditypk'
+        resp = requests.get(url,params={'id':'0513e390-1d4d-4a85-8f67-b499a6e7b943'})
+        commodity_list = resp.json().get('data')
+        print(commodity_list)
+
+    def test_commodityfk(self):
+        url = 'http://localhost:8000/goods/commodityfk'
+        resp = requests.get(url,params={'id':'34c19d99-19a6-4e86-9144-18a8670cc577'})
+        commodity_list = resp.json().get('data')
+        print(commodity_list)
+
+
+
--- goods/views.py
+++ goods/views.py
@@ -7,17 +7,18 @@ class CommodityViewALL(APIView):  #所有商品信息
     def get(self,request):
         datas = CommodityModel.objects.all()
         serializer = CommodityModelSerializer(datas, many=True) #序列化类
-        return Response(serializer.data)
-
+        return Response({'data': serializer.data})
 
 class CommodityViewPK(APIView):   #通过商品id来获取商品信息
-    def get(self,request,id):
-        datas = CommodityModel.objects.filter(pk=id).all()
+    def get(self,request):
+        pid=request.GET.get('id')
+        datas = CommodityModel.objects.filter(id=pid).all()
         serializer = CommodityModelSerializer(datas, many=True) #序列化类
-        return Response(serializer.data)
+        return Response({'data': serializer.data})
 
-class CommodityModelViewFK(APIView):  #通过商品类型获取商品信息
-    def get(self,request,categoryId):
-        datas = CommodityModel.objects.filter(categoryId_id=categoryId)
+class CommodityViewFK(APIView):  #通过商品类型获取商品信息
+    def get(self,request):
+        fid = request.GET.get('id') #获取用户请求的参数
+        datas = CommodityModel.objects.filter(categoryId_id=fid).all() #当用户请求的参数和外键的id值相等时返回所有数据
         serializer = CommodityModelSerializer(datas, many=True) #序列化类
-        return Response(serializer.data)
+        return Response({'data': serializer.data})
