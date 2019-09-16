9cbd516fea17b9dc0bf307e91257e63fe3e9e3c5
GunnWong <25360051@qq.com>
2019-09-16 13:25:57 +0800

    1

--- goods/test_/test_category.py
+++ goods/test_/test_category.py
@@ -3,9 +3,6 @@ import unittest
 from unittest import TestCase, TestSuite, TextTestRunner
 
 
-
-
-
 class UserTestCase(TestCase):
     def test_01_category(self):
         url = 'http://localhost:8000/goods/f_category'
@@ -13,14 +10,13 @@ class UserTestCase(TestCase):
         category_list = resp.json().get('data')
         print(category_list)
 
-
-
     def test_02_category(self):
         url = 'http://localhost:8000/goods/s_category/ad7f227d-73c0-44a2-9edd-924006deb134'
         resp = requests.get(url)
 
         category_list1 = resp.json().get('data')
         print(category_list1)
+
     def test_03_category(self):
         url = 'http://localhost:8000/goods/category_list'
         resp = requests.get(url)
@@ -34,9 +30,10 @@ class UserTestCase(TestCase):
 
         category_list = resp.json().get('data')
         print(category_list)
+
     def test_04_search(self):
         url = 'http://localhost:8000/goods/search'
-        resp = requests.get(url,params={"wd":"李"})
+        resp = requests.get(url, params={"wd": "李"})
 
         search_list = resp.json().get('data')
         print(search_list)
