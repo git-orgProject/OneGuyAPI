fda68ed5fdd4baa3e3f5bfc38e183778e48d1dcc
Ivan-xia <875634410@qq.com>
2019-09-16 09:12:46 +0800

    修改了

--- goods/urls.py
+++ goods/urls.py
@@ -1,7 +1,5 @@
-
 from django.urls import path
 from .views import CommodityViewALL,CommodityViewPK,CommodityViewFK
-
 
 
 
@@ -19,32 +17,14 @@ urlpatterns = [
             path('api/<str:pk>', category_api_detail, name='category'),
             path('api/',CategoryAPIView.as_view(), name='category_1'),
             path('f_category',FirstCategoryView.as_view(),name="first_category"),
-<<<<<<< HEAD
-            path('commodity/',CommodityViewALL.as_view(),name='commodity_all'),
 
-            path('commodity/<str:id>',CommodityViewPK.as_view(),name='commodity_pk'),
-=======
->>>>>>> e7e427a2e5e25056c3a5de453e0f4693ff694644
             path('s_category/<str:fk>',SecondCategoryView.as_view(),name="second_category"),
             path('coupon/<str:userid>',CouponView.as_view(),name='CouponView'),
             path('category_list',product_category_list),
 
-<<<<<<< HEAD
-
-            path('commoditypk',CommodityViewPK.as_view(),name='commodity_pk'),
-            path('commodityfk',CommodityViewFK.as_view(),name='commodity_fk'),
-
-            path('coupon/<str:userid>',CouponView.as_view(),name='CouponView')
-
-
-
-=======
             path('commodity/', CommodityViewALL.as_view(), name='commodity_all'),
             path('commoditypk',CommodityViewPK.as_view(),name='commodity_pk'),
             path('commodityfk',CommodityViewFK.as_view(),name='commodity_fk'),
-
-
->>>>>>> e7e427a2e5e25056c3a5de453e0f4693ff694644
 
 ]
 
